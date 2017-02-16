533484e164685dc2bd6c84d848953fc8769c4627
Shreyansh Zazz <shrey.binary@gmail.com>
2017-02-16 11:16:08 +0530

    Events Category Added

--- js/main.js
+++ js/main.js
@@ -2,8 +2,5 @@ $(document).ready(function() {
      $('#fullpage').fullpage({
        anchors: ['techveda', 'competitions'],
        scrollOverflow: true,
-       navigation: true,
-       navigationPosition: 'right',
-       navigationTooltips: ['TechVeda 2k17', 'Browse Competitions']
      });
  });
