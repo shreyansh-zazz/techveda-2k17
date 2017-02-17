de431b7dbe3dc16c5abbbe4159d480acea962c80
Shreyansh Zazz <shrey.binary@gmail.com>
2017-02-17 19:04:34 +0530

    Technical Events Added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-     $('#fullpage').fullpage({
-       anchors: ['techveda', 'competitions'],
-       scrollOverflow: true,
-     });
+
+  // Preload all images.
+  imagesLoaded(document.querySelector('main'), function() {
+    document.body.classList.remove('loading');
+  });
+
+
+
+ $('#fullpage').fullpage({
+   anchors: ['techveda', 'competitions'],
+   scrollOverflow: true
+ });
+
+ $('.category-wrapper a').click(function(){
+   $(this).siblings().removeClass('on');
+   $(this).addClass('on');
+   var title = $(this).data('which');
+   console.log(title);
+   $('.page').removeClass('showing');
+   $('.'+title).addClass('showing');
+   $.fn.fullpage.reBuild();
+ });
+
  });
